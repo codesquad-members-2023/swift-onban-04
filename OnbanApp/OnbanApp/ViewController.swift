6ebdba576de2b89607f24b14dabddd346dd8abe7
dpfdlalfm <84387335+dpfdlalfm@users.noreply.github.com>
2023-04-19 16:41:32 +0900

    [add] 홈 화면 UI CollectionView Cell 구성
    - 요구사항에 맞춰 Cell UI요소들 Constraint 추가 및 적용
    - CustomCell 클래스 생성
    Why? 스토리보드로 셀 내 요소들 연결 구현

--- OnbanApp/OnbanApp/ViewController.swift
+++ OnbanApp/OnbanApp/ViewController.swift
@@ -2,10 +2,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var collectionView: UICollectionView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
+
     }
-    
-    
 }
 
+extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as? CustomCell else {
+            return UICollectionViewCell()
+        }
+        
+        // 디자인 가이드라인에 맞춰 크기를 설정했지만
+        // X와 Y 좌표는 임의로 설정했으며 추가로 수정할 계획입니다.
+        cell.foodImage.frame = CGRect(x: 0, y: 0, width: 130, height: 130)
+        cell.foodImage.backgroundColor = .green
+
+        return cell
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: self.view.bounds.width, height: 130)
+    }
+}
