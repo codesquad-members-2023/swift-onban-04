9fca22abc682e59828251035ed4fa135a4e31af5
dpfdlalfm <84387335+dpfdlalfm@users.noreply.github.com>
2023-05-05 18:19:47 +0900

    NotificationName 구조체 작성
    - Notification 주고받을 때 알림명으로 사용될 타입 프로퍼티 작성
    Why? Observer / Publisher 등록간 불필요한 코드 길어짐을 방지

---
+++ OnbanApp/OnbanApp/NotificationName.swift
@@ -0,0 +1,9 @@
+//  Created by 1 on 2023/05/04.
+
+import Foundation
+
+struct NotificationName {
+    static let homeMain = Notification.Name("Home 화면의 메인 데이터가 로딩 완료되었습니다.")
+    static let homeSoup = Notification.Name("Home 화면의 국물 데이터가 로딩 완료되었습니다.")
+    static let homeSide = Notification.Name("Home 화면의 반찬 데이터가 로딩 완료되었습니다.")
+}
